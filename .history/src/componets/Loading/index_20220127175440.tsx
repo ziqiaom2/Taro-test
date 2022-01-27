fbd9af73a1f43d1711685761653f5966a7b6dfba
rexm <ziqiaom2@outlook.com>
2022-01-27 18:55:31 +0800

    edit completed

---
+++ .history/src/componets/Loading/index_20220127175440.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View } from 'taro';
+;
+
+export default function Loading (){
+
+		return (
+			<View>
+				<h1 style={{backgroundColor:'gray',color:'pink'}}>Loading....</h1>
+        </View>
+		)
+	}
---
+++ src/componets/Loading/index.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View } from '@tarojs/components';
+
+
+export default function Loading (){
+
+		return (
+			<View>
+				<View style={{backgroundColor:'gray',color:'pink'}}>Loading....</View>
+        </View>
+		)
+	}
